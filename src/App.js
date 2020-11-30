b1541335060f11aba32a20faf6bbdd691a62c6ce
simonacossai <63307173+simonacossai@users.noreply.github.com>
2020-11-30 11:48:34 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends React.Component {
+
+  render(){
   return (
     <div className="App">
       <header className="App-header">
@@ -20,6 +23,7 @@ function App() {
       </header>
     </div>
   );
+  }
 }
 
 export default App;
