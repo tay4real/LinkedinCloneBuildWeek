f96280a7f8548a78d057313b9b5bcc9e57685276
simonacossai <63307173+simonacossai@users.noreply.github.com>
2020-11-30 17:12:35 +0100

    DONE- adding profile cards

--- src/Components/ProfileComponent.jsx
+++ src/Components/ProfileComponent.jsx
@@ -1,19 +1,25 @@
 import React, { Component } from 'react';
-import {Container, Row, Col} from 'react-bootstrap';
+import { Container, Row, Col, Card, ListGroup, Button } from 'react-bootstrap';
 import ProfileContainer from './ProfileContainer';
+import './Profile.css'
+import ModifyProfileCard from './ModifyProfileCard';
+import AnnounceCard from './AnnounceCard';
+
+
 export default class ProfileComponent extends Component {
     render() {
         return (
             <>
-            <Container>
-                <Row className="mt-5">
-                    <Col md={8} style={{height:"500px", backgroundColor:"red"}}>
-                    <ProfileContainer/>
-                    </Col>
-                    <Col md={4} style={{height:"500px", backgroundColor:"blue"}}>
-
-                    </Col>
-                </Row>
+                <Container>
+                    <Row className="mt-5">
+                        <Col md={8} style={{}}>
+                            <ProfileContainer />
+                        </Col>
+                        <Col md={4} style={{flexDirection:"column", display:"flex", justifyContent:"start", alignItems:"center", textAlign:"center"}} className="mt-2 ">
+                           <ModifyProfileCard/>
+                           <AnnounceCard/>
+                        </Col>
+                    </Row>
                 </Container>
             </>
         )
--- src/Components/ProfileContainer.jsx
+++ src/Components/ProfileContainer.jsx
@@ -8,7 +8,7 @@ export default class ProfileContainer extends Component {
     render() {
         return (
 
-            <Card>
+            <Card className="card-user-profile">
                 <Card.Img variant="top"
                     src="https://images.unsplash.com/photo-1489875347897-49f64b51c1f8?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80"
                     className="coverImage image-fluid" />
@@ -30,9 +30,12 @@ export default class ProfileContainer extends Component {
                         <a className="p-0 m-0 ml-2">More info</a>
                     </Card.Text>
                 </Card.Body>
-                <Card className="mx-3 p-0 mb-2">
-                    <Card.Header as="p" className="text-left p-1 m-0">Open to work
-                    <p>Frontend web developer roles</p></Card.Header>
+                <Card className="mx-3 p-0 mb-4">
+                    <Card.Header as="p" className="text-left p-2 m-0">
+                        <div className="d-flex justify-content-between">
+                        <p className="p-0 m-0">Open to work</p> <FaPencilAlt className="text-right"/>
+                    </div>
+                    <p className="p-0 m-0">Frontend web developer roles</p></Card.Header>
                     <Card.Body className="p-0 m-0">
                         <Card.Text className="p-0 m-0 d-flex align-items-center">
                            <AiFillEye className="p-0 my-0 text-black mx-2 d-flex align-items-center"/> <p className="p-0 m-0 text-left">Only recruiters</p>
