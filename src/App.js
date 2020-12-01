b6e52c983d26ca9f32f2b2b413af7a36e1b9353b
ErlensGadzans <erlens.gadzans@users.noreply.github.com>
2020-12-01 12:26:22 +0200

    Updated Interests and experience components

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import React from 'react';
 import './App.css';
 import {BrowserRouter as Router, Route} from "react-router-dom"
 import Footer from "./components/Footer";
-
 import 'bootstrap/dist/css/bootstrap.min.css';
 import logo from './logo.svg';
 import './App.css';
-import NavBar from './Components/Navbar';
+import NavBar from './components/Navbar';
+import Experience from './components/Experience';
+import Interests from './components/Interests';
 
 
 class App extends React.Component {
@@ -18,11 +19,14 @@ class App extends React.Component {
     <div className="App">
 
       <Router>
-      
+      <NavBar />
+      <Route path="/" exact component={Experience} />
+      <Route path="/" exact component={Interests} />
       <Route path="/" exact component={Footer} />
+   
    </Router>
 
-     <NavBar />
+     
 
     </div>
   );
