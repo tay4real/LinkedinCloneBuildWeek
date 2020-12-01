a7bc23bf89346b171b645f1d8324b52b043373ad
Otubusin Ademuyiwa <53128146+tay4real@users.noreply.github.com>
2020-12-01 22:46:30 +0100

    Update SideCard.jsx

--- src/components/SideCard.jsx
+++ src/components/SideCard.jsx
@@ -3,7 +3,7 @@ import { Row, Col, Image} from 'react-bootstrap'
 import Description from './Description'
 import ImageContainer from './ImageContainer'
 import Title from './Title'
-import {BiUserPlus} from 'react-icons'
+import {CgUserAdd} from 'react-icons/cg'
 
 
 
@@ -20,15 +20,16 @@ export default class SideCard extends Component {
                     
                     <Col md={3} ><ImageContainer imgSrc={this.props.imgSrc} /></Col>
                     <Col md={9} >
-                        <Row className="justify-content-center align-items-center border-bottom border-secondary">
+                        <Row className="justify-content-center align-items-center border-bottom border-secondary p-0 m-0">
                             <Col md={10} >
                                 <Title title={this.props.title}/>
                                 <Description description={this.props.description} />
                             </Col>
-                            <Col md={2} className="rounded border border-secondary rounded-circle d-flex ">
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" data-supported-dps="16x16" fill="currentColor" className="mercado-match m-auto" height="16" focusable="false">
-                                    <path d="M9 4a3 3 0 11-3-3 3 3 0 013 3zM6.75 8h-1.5A2.25 2.25 0 003 10.25V15h6v-4.75A2.25 2.25 0 006.75 8zM13 8V6h-1v2h-2v1h2v2h1V9h2V8z"></path>
-                                </svg>
+                            <Col md={2}  >
+                                <span className="border border-secondary rounded-circle d-flex p-0 mr-5" style={{width:"20px" , height : "20px", marginRight: "5px"}}>
+                                    <CgUserAdd className="m-auto" />
+                                </span>
+                                
                             </Col>
                         </Row>
                         
