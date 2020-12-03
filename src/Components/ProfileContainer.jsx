5fa182cae7eeaa98c73933c1af76ef8039f00f45
simonacossai <63307173+simonacossai@users.noreply.github.com>
2020-12-03 21:23:18 +0100

    DONE-adding the possibility to upload a profile pic

--- src/Components/ProfileContainer.jsx
+++ src/Components/ProfileContainer.jsx
@@ -10,6 +10,7 @@ export default class ProfileContainer extends Component {
 
     state={
         user: [],
+        profile:[]
     }
     getUserProfile=async()=>{
         let response = await fetch("https://striveschool-api.herokuapp.com/api/profile/me",{
@@ -26,14 +27,52 @@ export default class ProfileContainer extends Component {
     componentDidMount(){
         this.getUserProfile();
     }
+
+    HandleFile = (e) => {
+        const formData = new FormData();
+        formData.append("profile", e.target.files[0]);
+        console.log(process.env.REACT_APP_API_TOKEN)
+        console.log(formData);
+        this.setState({ profile: formData });
+    };
+    PostImage = async () => {
+        try {
+            let response = await fetch(
+                `https://striveschool-api.herokuapp.com/api/profile/${process.env.REACT_APP_USER_ID}/picture`,
+                {
+                    method: "POST",
+                    body: this.state.profile,
+                    headers: {
+                        "Authorization": `Bearer ${process.env.REACT_APP_API_TOKEN}`,
+                    },
+                }
+            );
+            if (response.ok) {
+                console.log("OK");
+                console.log("ueueue", this.state.profile)
+                this.getUserProfile()
+            } else {
+                const error = await response.json();
+                console.log(error);
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    };
     render() {
         return (
           <Card className="card-user-profile">
                 <Card.Img variant="top"
                     src="https://media-exp1.licdn.com/dms/image/C4E16AQFGvXwGqrpN7w/profile-displaybackgroundimage-shrink_350_1400/0/1602886552408?e=1612396800&v=beta&t=OgyPkQ41f4wnIJJ0SWmEY4caxBbVbzSd_o0i7MplkFk"
                     className="coverImage image-fluid" />
+                 <input
+                      type="file"
+                      id="profile-pic"
+                      onChange={this.HandleFile}
+                    />
+                <button onClick={this.PostImage}>upload</button>
                 <Card.Body>
-                    <img src="https://media-exp1.licdn.com/dms/image/C4D03AQFQbLFj5Hs2kw/profile-displayphoto-shrink_400_400/0?e=1612396800&v=beta&t=ZqwAjRdb3l6vw76BXdUMU2UT5D-bPni7LqbahbQVVc0" className="profile-image" />
+                    <img src={this.state.user.image ?? "https://media-exp1.licdn.com/dms/image/C4D03AQFQbLFj5Hs2kw/profile-displayphoto-shrink_400_400/0?e=1612396800&v=beta&t=ZqwAjRdb3l6vw76BXdUMU2UT5D-bPni7LqbahbQVVc0"} className="profile-image" />
                     <div className="buttons">
                         <Button className="add-profile-button mr-2">Add profile section <RiArrowDownSFill className="m-0 p-0" style={{ fontSize: "17px" }} /></Button>
                         <Button className="more-button mr-1 py-0">More...</Button>
