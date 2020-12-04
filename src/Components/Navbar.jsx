8e078b4b16764e76927ef8c06acb837e2e82b877
Otubusin Ademuyiwa <53128146+tay4real@users.noreply.github.com>
2020-12-04 15:02:23 +0100

    fix reloading issues

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -51,7 +51,7 @@ class NavBar extends Component {
           expand="lg"
         >
           <Container>
-            <Navbar.Brand href="#home">
+            <Navbar.Brand to="#home">
               <img src={logo} alt="logo" className="nav-brand" />
             </Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
@@ -80,19 +80,19 @@ class NavBar extends Component {
                     <p>Home</p>
                   </div>
                 </Link>
-                <Nav.Link href="#" className="nav-link">
+                <Nav.Link to="#" className="nav-link">
                   <FaUserFriends />
                   <p>My Network</p>
                 </Nav.Link>
-                <Nav.Link href="#" className="nav-link">
+                <Nav.Link to="#" className="nav-link">
                   <BsFillBriefcaseFill />
                   <p>Jobs</p>
                 </Nav.Link>
-                <Nav.Link href="#" className="nav-link">
+                <Nav.Link to="#" className="nav-link">
                   <RiMessage2Fill />
                   <p>Messaging</p>
                 </Nav.Link>
-                <Nav.Link href="#" className="nav-link">
+                <Nav.Link to="#" className="nav-link">
                   <RiNotification3Fill />
                   <p>Notifications</p>
                 </Nav.Link>
@@ -101,79 +101,86 @@ class NavBar extends Component {
                     cssClass="e-caret-hide"
                     menuAlign="right"
                     variant="transparent"
-                    href="#"
+                    to="#"
                     className="nav-link"
                   >
                     <img src={logo} className="profile-img" />
                     <p>
-                      Me{" "}
-                      <RiArrowDownSFill
-                        className="m-0 p-0"
-                        style={{ fontSize: "17px" }}
-                      />
+                      Me {" "}
                     </p>
                   </Dropdown.Toggle>
-                  <Dropdown.Menu>
-                    <Dropdown.Item href="/profile">
-                      <img
-                        width={10}
-                        height={10}
-                        className="align-self-left mr-2"
-                        id="profile-image"
-                        style={{ borderRadius: "50%" }}
-                        src={this.state.user.image}
-                        alt="Generic placeholder"
-                      />
-                      <b>
-                        {this.state.user.name} {this.state.user.surname}
-                      </b>
+                  <Dropdown.Menu >
+                    <Dropdown.Item >
+                      <Link to="/profile">
+                          <img
+                          width={10}
+                          height={10}
+                          className="align-self-left mr-2"
+                          id="profile-image"
+                          style={{ borderRadius: "50%" }}
+                          src={this.state.user.image}
+                          alt="Generic placeholder"
+                        />
+                        <b>
+                          {this.state.user.name} {this.state.user.surname}
+                        </b>
+                      </Link>
                     </Dropdown.Item>
-
-                    <Button
-                      className="request-announce-button"
-                      style={{ width: "100%" }}
-                      href="/profile"
-                    >
-                      View Profile
-                    </Button>
+                    
+                     
+                    <Dropdown.Item to="#/action-3">
+                      <Link  to="/profile" ><Button className="request-announce-button" style={{ width: "100%" }}>View Profile</Button></Link>
+                    </Dropdown.Item>
+                   
+        
                     <Dropdown.Divider />
-                    <Dropdown.Item href="#/action-3">
+                    <Dropdown.Item to="#/action-3">
                       <b>Account</b>
                     </Dropdown.Item>
-                    <Dropdown.Item href="#/action-3">
+                    <Dropdown.Item to="#/action-3">
                       Access My Premium
                     </Dropdown.Item>
-                    <Dropdown.Item href="#/action-3">
+                    <Dropdown.Item to="#/action-3">
                       Settings & Privacy
                     </Dropdown.Item>
-                    <Dropdown.Item href="#/action-3">Help</Dropdown.Item>
-                    <Dropdown.Item href="#/action-3">Language</Dropdown.Item>
+                    <Dropdown.Item to="#/action-3">Help</Dropdown.Item>
+                    <Dropdown.Item to="#/action-3">Language</Dropdown.Item>
                     <Dropdown.Divider />
-                    <Dropdown.Item href="#/action-3">
+                    <Dropdown.Item to="#/action-3">
                       <b>Manage</b>
                     </Dropdown.Item>
-                    <Dropdown.Item href="#/action-3">
+                    <Dropdown.Item to="#/action-3">
                       Posts & Acitivties
                     </Dropdown.Item>
 
                     <Dropdown.Divider />
-                    <Dropdown.Item href="#/action-3">Sing Out</Dropdown.Item>
+                    <Dropdown.Item to="#/action-3">Sign Out</Dropdown.Item>
                   </Dropdown.Menu>
                 </Dropdown>
               </Nav>
               <Nav className="second-nav">
-                <Nav.Link href="#" className="nav-link">
-                  <CgMenuGridR />
-                  <p>
-                    Work{" "}
-                    <RiArrowDownSFill
-                      className="m-0 p-0"
-                      style={{ fontSize: "17px" }}
-                    />
-                  </p>
+                
+                 
+                <Nav.Link to="#" className="nav-link">
+                  <Dropdown>
+                      <Dropdown.Toggle
+                        cssClass="e-caret-hide"
+                        menuAlign="right"
+                        variant="transparent"
+                        to="#"
+                        className="nav-link"
+                      >
+                          <CgMenuGridR />
+                          <p>
+                            Work{" "}
+                          
+                          </p>
+                      </Dropdown.Toggle>
+                    </Dropdown>
                 </Nav.Link>
+
                 <Nav.Link
-                  href="#"
+                  to="#"
                   className="nav-link"
                   style={{ maxWidth: "70px", textDecoration: "underline" }}
                 >
